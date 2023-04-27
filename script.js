00631a842bc47f9cda183ea20a78f7630ea2e485
Falcao <113912350+falcaoghost@users.noreply.github.com>
2023-04-27 15:42:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.container__titulo', { duration: 1000 });
+
+sr.reveal('.descricao__titulo', { rotate: { x: 0, y: 80, z: 0}, duration: 2000 });
+  
+sr.reveal('.descricao__texto', { rotate: { x: 0, y: 80, z: 0}, duration: 2000 });
+
